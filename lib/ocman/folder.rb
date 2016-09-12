7a28dc8e44c2e828209a0b9aeb5a1633067bb60d
Waldemar <wg@toptranslation.com>
2016-09-12 14:36:32 +0200

    handle URI encode and decode

--- lib/ocman/folder.rb
+++ lib/ocman/folder.rb
@@ -3,15 +3,15 @@ require 'hashie'
 module Ocman
   class Folder
     def self.create(path)
-      Ocman::Dav.new.mkdir(path)
+      Ocman::Dav.new.mkdir(URI.encode(path))
     end
 
     def self.list(path)
       accu = []
 
-      Ocman::Dav.new.ls(path) do |item|
+      Ocman::Dav.new.ls(URI.encode(path)) do |item|
         accu << Hashie::Mash.new({
-          path: item.uri.to_s.gsub(Ocman::Dav.url(path), '').gsub('/', ''),
+          path: URI.decode(item.uri.to_s.gsub(Ocman::Dav.url(path), '').gsub('/', '')),
           type: item.type.to_s,
           size: item.size
         })
--- lib/ocman/item.rb
+++ lib/ocman/item.rb
@@ -1,15 +1,15 @@
 module Ocman
   class Item
     def self.create(file, path, options={})
-      Ocman::Dav.new.put(file, path, options)
+      Ocman::Dav.new.put(file, URI.encode(path), options)
     end
 
     def self.delete(path)
-      Ocman::Dav.new.delete(path)
+      Ocman::Dav.new.delete(URI.encode(path))
     end
 
     def self.move(source_path, destination_path)
-      Ocman::Dav.new.move(source_path, destination_path)
+      Ocman::Dav.new.move(URI.encode(source_path), URI.encode(destination_path))
     end
   end
 end
