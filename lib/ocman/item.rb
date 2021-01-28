3590197063be3db936d899fa7574b4f989a64164
sr189 <stefan.rohde@toptranslation.com>
2021-01-28 16:27:31 +0100

    fix: use ERB::Util.url_encode instead of CGI.escape

--- lib/ocman/item.rb
+++ lib/ocman/item.rb
@@ -3,15 +3,19 @@
 module Ocman
   class Item
     def self.create(file, path, options = {})
-      Ocman::Dav.new.put(file, CGI.escape(path), options)
+      Ocman::Dav.new.put(file, escape_path(path), options)
     end
 
     def self.delete(path)
-      Ocman::Dav.new.delete(CGI.escape(path))
+      Ocman::Dav.new.delete(escape_path(path))
     end
 
     def self.move(source_path, destination_path)
-      Ocman::Dav.new.move(CGI.escape(source_path), CGI.escape(destination_path))
+      Ocman::Dav.new.move(escape_path(source_path), escape_path(destination_path))
+    end
+
+    def self.escape_path(path)
+      path.split('/').map { |segment| ERB::Util.url_encode(segment) }.join('/')
     end
   end
 end
